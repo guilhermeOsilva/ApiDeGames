389bda87c982805f775afb208b9e91c8469822a9
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-09-24 08:40:44 -0300

    criando a model de game e usando o sync

---
+++ Model/gamesDb.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+const Game = sequelize.define("gamesDbs", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+  },
+  title: {
+    type: DataTypes.STRING,
+  },
+  year: {
+    type: DataTypes.TEXT,
+  },
+  price: {
+    type: DataTypes.DECIMAL(3, 2),
+  },
+});
+
+User.sync().then((response) => {
+  response.sendStatus(200);
+});
+
+
+module.exports = Game;
